f24f74373dde929c31c7f8b6d3a6300aa7decf5f
Tim Rowe <tim@tjsr.id.au>
2024-07-02 10:51:10 +1000

    Add lint to template.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 describe('Should pass with no tests', () => {
-    test('Passes', () => {
-        expect(true).toBe(true);
-    });
+  test('Passes', () => {
+    expect(true).toBe(true);
+  });
 });
